132732423fda853cada766c988458ce4a42e62bc
IndianCoder <akshayjoshi@gmail.com>
2022-05-22 18:08:52 +0530

    test CICD 3

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
   // Sending This is the home page! in the page
   res
     .status(200)
-    .send(`This is the home page and running on ${process.env.PORT} with smile :)`);
+    .send(`This is the home page and running on ${process.env.PORT}`);
 });
 
 // Listening to the port 
